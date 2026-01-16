9d9cb20b27ed41cbac455d10beea6f460fb4c28b
Sachin_Kumar_gupta <sachin1513r@gmail.com>
2026-01-16 13:04:41 +0000

    DOM

---
+++ 05_iteration/07_map.js
@@ -0,0 +1,14 @@
+const myNumers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+//map() creates a NEW array by transforming each element
+//using the value returned from the callback.
+
+
+ //const newNums = myNumers.map( (num) => { return num + 10})
+
+const newNums = myNumers
+                .map((num) => num * 10 )
+                .map( (num) => num + 1)
+                .filter( (num) => num >= 40)
+
+console.log(newNums);
